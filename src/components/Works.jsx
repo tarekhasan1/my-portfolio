28429963768aa5e2a5c5ba23a6a4d2f68e43ae82
Md. Tarek Hasan <tarekhasan.tkg101@gmail.com>
2023-07-09 20:53:19 +0600

    almost completed the design and some functionality

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styled from "styled-components";
+import Toytour from "./../../public/images/toy-tour.png";
 
 const Section = styled.div`
   min-height: 80vh;
@@ -10,7 +11,7 @@ const Title = styled.h2`
   text-align: center;
   margin-top: 50px;
   margin-bottom: 60px;
-`
+`;
 
 const WorksContainer = styled.div`
   display: flex;
@@ -48,9 +49,9 @@ const Card = styled.div`
   width: 100%;
   max-width: max-content;
   margin: 20px auto;
-  border: 1px solid #ccc;
+  border: none;
   border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 4px 12px whitesmoke;
 `;
 
 const ImageContainer = styled.div`
@@ -116,9 +117,14 @@ const ViewDetailsButton = styled.button`
 `;
 
 const Modal = styled.div`
-  position: absolute;
-  top: 0;
+z-index: 99999999;
+  width: 75vw;
+  position: fixed;
+  padding: 10px;
+  border-radius: 5px;
+  top: 50%;
   left: 50%;
+  transform: translate(-50%, -50%);
   background-color: #ffffff;
   color: black;
 `;
@@ -137,86 +143,80 @@ const Works = () => {
   // Example data
   const projectsData = [
     {
-      id: 1,
-      name: "Project 1",
-      description: "Lorem ipsum dolor sit amet...",
-      technologies: ["React", "Node.js"],
+      name: "TOYTOUR || MARKETPLACE",
+      type: "-A complete MERN stack Project",
+      description:
+        "Interface made for consumers and sellers both and seller can add or update their      products anytime by logging in. fully functional car toy marketplace with a search option by name, category wise data",
+      technologies: [
+        "React",
+        "bootstrap",
+        "React Router",
+        "CSS",
+        "React-Bootstrap",
+        "Node.js",
+        "Express.js",
+        "MongoDB",
+      ],
       image:
-        "https://images.pexels.com/photos/2291428/pexels-photo-2291428.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      liveLink: "https://example.com/project1",
-      clientGithubLink: "https://github.com/client/project1",
-      serverGithubLink: "https://github.com/server/project1",
-      duration: "3 months",
+        "https://drive.google.com/file/d/1q8QgPvqQOwsDvQ4lN9ZQEMsHBQ4QdFdi/view?usp=sharing",
+      liveLink: "https://toy-tour.web.app/",
+      clientGithubLink: "https://github.com/tarekhasan1/toy-tour",
+      serverGithubLink: "https://github.com/tarekhasan1/toy-tour-server",
+      duration: "4 Days",
     },
     {
-      id: 1,
-      name: "Project 1",
-      description: "Lorem ipsum dolor sit amet...",
-      technologies: ["React", "Node.js"],
+      name: "EPICUREAN EDGE || THAI CHEFS AND FOODS",
+      type: "-A complete ReactJS Project",
+      description:
+        "This is a Thai food restaurant website with popular chefs and their recipes. Implented Protected routing. Fully responsive website with firebase authentication and data taken from own api",
+      technologies: [
+        "React",
+        "bootstrap",
+        "React Router",
+        "CSS",
+        "React-Bootstrap",
+        "Node.js",
+        "Express.js",
+      ],
       image:
         "https://images.pexels.com/photos/2291428/pexels-photo-2291428.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      liveLink: "https://example.com/project1",
-      clientGithubLink: "https://github.com/client/project1",
-      serverGithubLink: "https://github.com/server/project1",
-      duration: "3 months",
+      liveLink: "https://epicurean-edge.web.app/",
+      clientGithubLink: "https://github.com/tarekhasan1/epicurean-edge",
+      serverGithubLink: "https://github.com/tarekhasan1/epicurean-edge-server",
+      duration: "3 days",
     },
     {
-      id: 1,
-      name: "Project 1",
-      description: "Lorem ipsum dolor sit amet...",
-      technologies: ["React", "Node.js"],
+      name: "MELODIC MASTERY || A MUSIC INSTRUMENTAL SUMMER SCHOOL",
+      type: "-A complete MERN stack Project",
+      description:
+        "A simple animated website containing music courses of different instructors, and different Dashboard for Students, Instructors and Admin. firebase login system available for all kinds of users, Payment option and manage users. and classes from UI functionality added Student can enroll in Classes, Instructors can add their Classes and Admin can manage everything about Student and Instructors",
+      technologies: [
+        "React",
+        "bootstrap",
+        "React Router",
+        "CSS",
+        "React-Bootstrap",
+        "framer-motion",
+        "AOS",
+        "Node.js",
+        "Express.js",
+        "MongoDB",
+      ],
       image:
         "https://images.pexels.com/photos/2291428/pexels-photo-2291428.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      liveLink: "https://example.com/project1",
-      clientGithubLink: "https://github.com/client/project1",
-      serverGithubLink: "https://github.com/server/project1",
-      duration: "3 months",
+      liveLink: "https://melodic-mastery-6b24a.web.app/",
+      clientGithubLink: "https://github.com/tarekhasan1/melodic-mastery-client",
+      serverGithubLink: "https://github.com/tarekhasan1/melodic-mastery-server",
+      duration: "10 days",
     },
-    {
-      id: 1,
-      name: "Project 1",
-      description: "Lorem ipsum dolor sit amet...",
-      technologies: ["React", "Node.js"],
-      image:
-        "https://images.pexels.com/photos/2291428/pexels-photo-2291428.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      liveLink: "https://example.com/project1",
-      clientGithubLink: "https://github.com/client/project1",
-      serverGithubLink: "https://github.com/server/project1",
-      duration: "3 months",
-    },
-    {
-      id: 1,
-      name: "Project 1",
-      description: "Lorem ipsum dolor sit amet...",
-      technologies: ["React", "Node.js"],
-      image:
-        "https://images.pexels.com/photos/2291428/pexels-photo-2291428.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      liveLink: "https://example.com/project1",
-      clientGithubLink: "https://github.com/client/project1",
-      serverGithubLink: "https://github.com/server/project1",
-      duration: "3 months",
-    },
-    {
-      id: 1,
-      name: "Project 1",
-      description: "Lorem ipsum dolor sit amet...",
-      technologies: ["React", "Node.js"],
-      image:
-        "https://images.pexels.com/photos/2291428/pexels-photo-2291428.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      liveLink: "https://example.com/project1",
-      clientGithubLink: "https://github.com/client/project1",
-      serverGithubLink: "https://github.com/server/project1",
-      duration: "3 months",
-    },
-    // Add more projects here
   ];
 
   return (
     <Section id="works">
       <Title>My Projects</Title>
       <WorksContainer>
-        {projectsData.slice(0, 3).map((project) => (
-          <Card key={project.id}>
+        {projectsData.slice(0, 3).map((project, idx) => (
+          <Card key={idx}>
             <ImageContainer>
               <CardImage
                 src={project.image}
@@ -240,8 +240,37 @@ const Works = () => {
           <Modal>
             {/* Modal content with project details */}
             <h2>{selectedProject.name}</h2>
-            {/* Add other project details here */}
-            <button onClick={closeModal}>Close</button>
+            <h5>{selectedProject.type}</h5>
+            <p>{selectedProject.description}</p>
+            <h4>Technology:</h4>
+            <ul style={{marginBottom: "15px"}}>
+              {selectedProject.technologies.map((technology) => (
+                <li key={technology}>{technology}</li>
+              ))}
+            </ul>
+            <a style={{marginRight: "7px", textDecoration: "none"}} target="_blank" href={selectedProject.liveLink}>
+              Live Preview
+            </a>
+            <a style={{marginRight: "7px", textDecoration: "none"}} target="_blank" href={selectedProject.clientGithubLink}>
+              Github Client
+            </a>
+            <a style={{marginRight: "7px", textDecoration: "none"}} target="_blank" href={selectedProject.serverGithubLink}>
+              Github Server
+            </a>
+            <br />
+            <button
+              style={{
+                color: "red",
+                marginBottom: "5px",
+                border: "none",
+                borderRadius: "4px",
+                padding: "5px",
+                marginTop: "10px",
+              }}
+              onClick={closeModal}
+            >
+              Close
+            </button>
           </Modal>
         )}
       </WorksContainer>
